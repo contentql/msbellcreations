483dc0bdf26cbe9dc7ddee5f9d9621d7add382fc
jagadeeesh507 <jagadeeshm778@gmail.com>
2024-01-19 11:59:22 +0530

    chore: mainUrl image added for home page

--- src/sections/_ecommerce/product/item/ecommerce-product-item-hero.js
+++ src/sections/_ecommerce/product/item/ecommerce-product-item-hero.js
@@ -30,7 +30,7 @@ export default function EcommerceProductItemHero({ products, current }) {
         pt: 12,
         pb: 10,
         px: { xs: 3, md: 10 },
-        backgroundColor: `${product.bgColor}`,
+        backgroundColor: `${product?.bgColor}`,
       }}
     >
       <Grid xs={12} md={6}>
@@ -41,20 +41,20 @@ export default function EcommerceProductItemHero({ products, current }) {
           }}
         >
           <Label color="warning" sx={{ mb: 2 }}>
-            {product.label || 'In Stock'}
+            {product?.label || 'In Stock'}
           </Label>
 
           <TextMaxLine variant="h3" sx={{ mb: 2 }}>
-            {product.name}
+            {product?.name}
           </TextMaxLine>
 
           <TextMaxLine line="3" variant="body2" sx={{ mb: 5, color: 'text.secondary' }}>
-            {product.caption}
+            {product?.caption}
           </TextMaxLine>
 
           <Button
             component={RouterLink}
-            href={`${paths.eCommerce.products}/${product.id}`}
+            href={`${paths.eCommerce.products}/${product?.id}`}
             size="large"
             color="inherit"
             variant="contained"
@@ -67,7 +67,7 @@ export default function EcommerceProductItemHero({ products, current }) {
 
       <Grid xs={12} md={6}>
         <Image
-          src={product.coverUrl.url}
+          src={product.mainurl !== null ? product?.mainurl?.url : product.coverUrl.url}
           sx={{
             filter: `drop-shadow(20px 20px 24px ${alpha(theme.palette.common.black, 0.16)})`,
             maxWidth: 400,
@@ -84,6 +84,7 @@ EcommerceProductItemHero.propTypes = {
   products: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
+      url: PropTypes.string,
       name: PropTypes.string,
       caption: PropTypes.string,
       coverUrl: PropTypes.object,
